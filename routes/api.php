07daf65be6b991a52614d0ea79b849a3d3a7049e
TRIUMFO\Webdev <php@triumfo.de>
2025-04-16 11:19:06 +0530

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
+    Route::get('/user', [LoginController::class, 'user']);
+});
